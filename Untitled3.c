99037d88da64ee405271cc454c2a190f4c57b4b2
seelamsandeepkumarreddy <124854970+seelamsandeepkumarreddy@users.noreply.github.com>
2024-02-23 12:14:13 +0530

    Add files via upload

---
+++ Untitled3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    file*sourceFile=fopen("source.txt","r")
+    file*destFile=fopen("destination.txt","w")
+    char c;
+    
+    if(sourceFile== NULL || destiFile=NULL)
+    {
+    printf("source file cannot open");
+    exit(EXIT_FAILURE)
+}
+while((c=fgetc(sourceFile))!=EOF)
+{
+    fputc(c,destFile)
+}
+return 0;
+}
